688cc16e29492eec9a3917c2cb92746a74d33830
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2021-07-25 08:25:32 +0200

    working torwards cross-compilation

--- src/cumsum_f32/build.rs
+++ src/cumsum_f32/build.rs
@@ -1,11 +1,16 @@
 // Example custom build script.
 fn main() {
-    // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo:rerun-if-changed=src/ccumsum.c");
-    // Use the `cc` crate to build a C file and statically link it.
-    cc::Build::new()
-        .file("src/ccumsum.c")
-        .flag("-march=native")
-        .flag("-O3")
-        .compile("ccumsum.a");
+    let target_triple = std::env::var("TARGET").unwrap();
+
+    if target_triple.starts_with("x86") {
+        // Tell Cargo that if the given file changes, to rerun this build script.
+        println!("cargo:rerun-if-changed=src/ccumsum.c");
+        // Use the `cc` crate to build a C file and statically link it.
+
+        cc::Build::new()
+            .file("src/ccumsum.c")
+            .flag("-march=native")
+            .flag("-O3")
+            .compile("ccumsum.a");
+    }
 }
--- src/cumsum_f32/src/lib.rs
+++ src/cumsum_f32/src/lib.rs
@@ -26,14 +26,21 @@ mod cumsum_f32_avx_intrinsics;
 #[cfg(target_arch = "x86_64")]
 pub use cumsum_f32_avx_intrinsics::cumsum_f32_avx_intrinsics;
 
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 extern { fn cumsum_super_scaler_simd(array: *mut f32, len: usize);}
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub fn ccumsum(array: &mut [f32], len: usize){
     unsafe{
         cumsum_super_scaler_simd(   array.as_mut_ptr(), len)
     }
 }
 
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 extern { fn prefix_sum_AVX256(array: *mut f32, len: usize); }
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub fn the_stackoverflow_thingy_256(array: &mut [f32], len: usize) {
     unsafe{
         prefix_sum_AVX256(   array.as_mut_ptr(), len)
